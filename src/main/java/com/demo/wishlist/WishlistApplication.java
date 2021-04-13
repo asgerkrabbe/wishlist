dafe9f24f803861a4a7004cb28e3bdc08b95253c
Ines L <inesluparia@gmail.com>
2021-04-13 10:09:57 +0200

    dbmanager

--- src/main/java/com/demo/wishlist/WishlistApplication.java
+++ src/main/java/com/demo/wishlist/WishlistApplication.java
@@ -1,13 +1,17 @@
 package com.demo.wishlist;
 
+import com.demo.wishlist.data.DBManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.Connection;
 
 @SpringBootApplication
 public class WishlistApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WishlistApplication.class, args);
+
+        Connection conn = DBManager.getConnection();
     }
 
 }
