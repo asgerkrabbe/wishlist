ce9b9f88160cd8de19bcdaa267f18de82ca9be97
asgerkrabbe <Asgerkrabbe@gmail.com>
2021-04-13 12:21:15 +0200

    createuse() and login() finished

---
+++ src/main/java/com/demo/wishlist/data/LoginSampleException.java
@@ -0,0 +1,8 @@
+package com.demo.wishlist.data;
+
+public class LoginSampleException extends Exception {
+
+    public LoginSampleException(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/com/demo/wishlist/data/UserMapper.java
+++ src/main/java/com/demo/wishlist/data/UserMapper.java
@@ -1,6 +1,52 @@
 package com.demo.wishlist.data;
 
+import com.demo.wishlist.model.User;
+
+import java.sql.*;
+
 //Class in charge of mapping user data to DB
 public class UserMapper {
 
+    public void createUser(User user) throws LoginSampleException {
+        try {
+            Connection con = DBManager.getConnection();
+            String SQL = "INSERT INTO user_data (user_password, email, username) VALUES (?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getPassword());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getUserName());
+            ps.executeUpdate();
+
+            ResultSet ids = ps.getGeneratedKeys();
+            ids.next();
+            int id = ids.getInt(1);
+            user.setUserId(id);
+
+        } catch (SQLException ex) {
+            throw new LoginSampleException(ex.getMessage());
+        }
+    }
+
+    public User login(String email, String password) throws LoginSampleException {
+        try {
+            Connection con = DBManager.getConnection();
+            String SQL = "SELECT email, username, user_password, id FROM user_data "
+                    + "WHERE email=? AND user_password=?";
+            PreparedStatement ps = con.prepareStatement(SQL);
+            ps.setString(1, email);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                String username = rs.getString("username");
+                int id = rs.getInt("id");
+                User user = new User(email, username, password, id);
+                user.setUserId(id);
+                return user;
+            } else {
+                throw new LoginSampleException("Could not validate user");
+            }
+        } catch (SQLException ex) {
+            throw new LoginSampleException(ex.getMessage());
+        }
+    }
 }
