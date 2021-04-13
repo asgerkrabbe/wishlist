929181fe4665bb754844229d67910ab00bc92efa
asgerkrabbe <Asgerkrabbe@gmail.com>
2021-04-13 12:31:41 +0200

    createuse() and login() finished

--- src/main/java/com/demo/wishlist/data/UserMapper.java
+++ src/main/java/com/demo/wishlist/data/UserMapper.java
@@ -7,6 +7,12 @@ import java.sql.*;
 //Class in charge of mapping user data to DB
 public class UserMapper {
 
+    /**
+     * The method uses the user parameter in the SQL statement to create a new user.
+     * @param user
+     * @throws LoginSampleException
+     */
+
     public void createUser(User user) throws LoginSampleException {
         try {
             Connection con = DBManager.getConnection();
@@ -16,22 +22,30 @@ public class UserMapper {
             ps.setString(2, user.getEmail());
             ps.setString(3, user.getUserName());
             ps.executeUpdate();
-
             ResultSet ids = ps.getGeneratedKeys();
             ids.next();
             int id = ids.getInt(1);
             user.setUserId(id);
-
         } catch (SQLException ex) {
             throw new LoginSampleException(ex.getMessage());
         }
     }
 
+    /**
+     * Compares parameter email and password via SQL statement.
+     * If a match has been found, the if statement
+     * will return af user with email, username, password and ID for session use
+     * @param email
+     * @param password
+     * @return
+     * @throws LoginSampleException Custom made exception method. Without it, the "else" statement
+     *                              would not be possible.
+     */
     public User login(String email, String password) throws LoginSampleException {
         try {
             Connection con = DBManager.getConnection();
             String SQL = "SELECT email, username, user_password, id FROM user_data "
-                    + "WHERE email=? AND user_password=?";
+                        + "WHERE email=? AND user_password=?";
             PreparedStatement ps = con.prepareStatement(SQL);
             ps.setString(1, email);
             ps.setString(2, password);
