f59c4291ca651ebafe9bfc7dcb18194918806fa3
asgerkrabbe <Asgerkrabbe@gmail.com>
2021-04-14 10:12:36 +0200

    createuse() and login() finished

--- src/main/java/com/demo/wishlist/model/User.java
+++ src/main/java/com/demo/wishlist/model/User.java
@@ -40,4 +40,32 @@ public class User {
     public ArrayList<WishList> getWishLists() {
         return wishLists;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setWishLists(ArrayList<WishList> wishLists) {
+        this.wishLists = wishLists;
+    }
 }
