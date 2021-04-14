a31849431e5e3e5d5d9da8c139815d785bc9ee04
asgerkrabbe <Asgerkrabbe@gmail.com>
2021-04-14 10:40:03 +0200

    createuse() and login() finished

--- src/main/java/com/demo/wishlist/data/DBManager.java
+++ src/main/java/com/demo/wishlist/data/DBManager.java
@@ -16,7 +16,6 @@ public class DBManager {
     private static Connection connection = null;
 
     public static Connection getConnection() {
-
         if (connection != null) return connection;
         try (InputStream input = new FileInputStream("src/main/resources/application.properties")) {
             Properties properties = new Properties();
@@ -24,7 +23,6 @@ public class DBManager {
             url = properties.getProperty("url");
             user = properties.getProperty("user");
             password = properties.getProperty("password");
-
         } catch (IOException ex) {
             ex.printStackTrace();
         }
